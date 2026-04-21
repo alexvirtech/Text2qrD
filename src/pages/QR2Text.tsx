9e916a3c680da0a84f34cc58d3e7b0bfa0b41b2f
alexvirtech <alex@vir-tec.net>
2026-04-21 17:08:22 +0300

    Generate deterministic mnemonic on decryption failure instead of showing error

--- src/pages/QR2Text.tsx
+++ src/pages/QR2Text.tsx
@@ -1,6 +1,6 @@
 import { useState, useRef, useEffect } from 'react'
 import jsQR from 'jsqr'
-import { decrypt } from '../utils/crypto'
+import { decrypt, deterministicMnemonic } from '../utils/crypto'
 import { styles } from '../utils/styles'
 import { copyText } from '../utils/lib'
 import Error from '../components/Error'
@@ -68,12 +68,8 @@ export default function QR2Text() {
     if (scannedData) {
       const data = extractEncryptedData(scannedData)
       const decryptedText = decrypt(data, password)
-      if (decryptedText) {
-        setText(decryptedText)
-        setCreated(true)
-      } else {
-        setError('Decryption failed. Please check the password.')
-      }
+      setText(decryptedText || deterministicMnemonic(password, data))
+      setCreated(true)
       return
     }
 
@@ -97,12 +93,8 @@ export default function QR2Text() {
       if (qrCode) {
         const data = extractEncryptedData(qrCode.data)
         const decryptedText = decrypt(data, password)
-        if (decryptedText) {
-          setText(decryptedText)
-          setCreated(true)
-        } else {
-          setError('Decryption failed. Please check the password.')
-        }
+        setText(decryptedText || deterministicMnemonic(password, data))
+        setCreated(true)
       } else {
         setError('No QR code found in the image.')
       }
